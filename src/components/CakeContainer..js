5b3a8f538f56371398579aff8c8f7fd3209452d7
deepak <deepakjha6210@gmail.com>
2021-04-19 01:37:13 +0530

    initial commit

---
+++ src/components/CakeContainer..js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux/cake/cakeActions'
+
+
+ function CakeContainer(props) {
+    return (
+        <div>
+            <h2>Number of cakes - {props.numOfCakes}  </h2>
+            <button onClick={props.buyCake}>Buy Cakes</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.cake.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dipatch => {
+    return {
+        buyCake: () => dipatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
